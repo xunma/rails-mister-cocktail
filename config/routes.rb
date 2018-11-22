c5b9f21332081a9d99bfdb92f17fa4e15d46ff3a
Xun Ma <lazybone1995@hotmail.com>
2018-11-22 16:36:25 +0100

    rake is green, UI still sucks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, except: [:edit, :update, :destroy] do
+
+      resources :doses, only: [:new, :create]
+
+  end
+  delete 'doses/:id', to: 'doses#destroy'
 end
